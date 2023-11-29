3f1355bc719efe9352815b01588037c2620a8c64
Leonid Gogin <leonid.gogin@sdggroup.com>
2023-11-29 16:06:00 +0100

    Macke it works

--- config.py
+++ config.py
@@ -4,21 +4,35 @@ import os
 SLEEP_SEC = 30.0
 
 # csv output delimiter
-DELIM = ','
-HEADER_COLUMNS = ('place', 'url', 'scrape_time', 'day_of_week', 'hour_of_day', 'popularity_percent_normal', 'popularity_percent_current')
+DELIM = ","
+HEADER_COLUMNS = (
+    "place",
+    "url",
+    "scrape_time",
+    "day_of_week",
+    "hour_of_day",
+    "popularity_percent_normal",
+    "popularity_percent_current",
+)
 
 # path to chrome and chromedriver
-CHROME_BINARY_LOCATION = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
-CHROMEDRIVER_BINARY_LOCATION = '/usr/local/bin/chromedriver'
+# CHROME_BINARY_LOCATION = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+CHROME_BINARY_LOCATION = "/usr/bin/google-chrome"
+# CHROMEDRIVER_BINARY_LOCATION = '/usr/local/bin/chromedriver'
+CHROMEDRIVER_BINARY_LOCATION = (
+    "/home/leon/programs/SDG/test_google_map/chromedriver/stable/chromedriver"
+)
 
 # keep an cache of the source htmls, with a timestamp in the filename
 # if so, they should be cleaned out once in a while, since they are 1MB each
-SAVE_HTML = False
+SAVE_HTML = "/home/leon/programs/SDG/test_google_map/.cache/html"
 
 # put your url or path here to a csv where the first column is a google maps url
 # google sheets - export as csv https://stackoverflow.com/a/33727897/2327328
-URL_PATH_INPUT = None
-#URL_PATH_INPUT = 'urls.csv'
+URL_PATH_INPUT = (
+    "/home/leon/programs/SDG/test_google_map/gmaps_popular_times_scraper/test_urls.csv"
+)
+# URL_PATH_INPUT = 'urls.csv'
 
 DEBUG = False
-URL_PATH_INPUT_TEST = 'tests' + os.sep + 'test_urls.csv'
+URL_PATH_INPUT_TEST = "tests" + os.sep + "test_urls.csv"
